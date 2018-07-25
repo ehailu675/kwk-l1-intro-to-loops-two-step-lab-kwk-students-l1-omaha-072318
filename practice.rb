9aac54cfb2daf1f95d2a30a22e7a359bfd420545
ehailu675 <elisabethahailu@gmail.com>
2018-07-25 18:51:21 +0000

    automatically backed up by learn

--- practice.rb
+++ practice.rb
@@ -10,3 +10,13 @@
 # end 
 # puts "Do the stanky leg"
 # puts "Do the stanky leg"
+
+counter = 0
+
+loop do 
+  counter += 1
+  puts "Iteration #{counter} of the loop"
+  if counter >= 10 
+    break
+  end
+end
