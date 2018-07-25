756c6ce7eb9ab49964f0d61f491866109fa3deb8
ehailu675 <elisabethahailu@gmail.com>
2018-07-25 18:39:38 +0000

    automatically backed up by learn

--- practice.rb
+++ practice.rb
@@ -3,10 +3,10 @@ loop do
   puts "Watch me nae nae"
   puts "Now watch me whip, whip"
   puts "Now watch me nae nae"
-  puts "Ooooh, watch me, watch me"
-  puts "Ooooh, watch me, watch me"
-  puts "Ooooh, watch me, watch me"
   break
+  puts "Ooooh, watch me, watch me"
+  puts "Ooooh, watch me, watch me"
+  puts "Ooooh, watch me, watch me"
 end 
 puts "Do the stanky leg"
 puts "Do the stanky leg"
