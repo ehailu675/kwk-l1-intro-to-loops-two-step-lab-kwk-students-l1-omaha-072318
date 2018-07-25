d3c837060c9a6e787976734882ab3f27308dd8cb
ehailu675 <elisabethahailu@gmail.com>
2018-07-25 19:17:39 +0000

    automatically backed up by learn

--- practice.rb
+++ practice.rb
@@ -1,6 +1,6 @@
-num_of_hotdogs_eaten = 3
-  while num_of_hotdogs_eaten < 10
-    num_of_hotdogs_eaten += 2
-    puts "You have now eaten #{num_of_hotdogs_eaten} hot dog(s)."
-  end
-  puts "You ate a total of #{num_of_hotdogs_eaten} hot dogs!"
+# num_of_hotdogs_eaten = 3
+#   while num_of_hotdogs_eaten < 10
+#     num_of_hotdogs_eaten += 2
+#     puts "You have now eaten #{num_of_hotdogs_eaten} hot dog(s)."
+#   end
+#   puts "You ate a total of #{num_of_hotdogs_eaten} hot dogs!"
