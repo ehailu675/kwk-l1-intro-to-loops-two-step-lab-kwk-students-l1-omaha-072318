fc2023ef8904d2795c739b77c28c4eb1cf118224
ehailu675 <elisabethahailu@gmail.com>
2018-07-25 19:30:02 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -35,7 +35,14 @@ def a_few_more_steps
   # Write a loop that outputs the first two sets of steps in the Two-Step
 end
 
-def how_many_steps?
+def how_many_steps
+  steps = 0
+  loop do
+    steps += 1
+    puts "Left" if steps % 2 = 0
+    puts "Right" if steps % 2 =! 0
+  sleep(0.5)
+end
   # Write a loop that fulfills all criteria from Part 3 of the README.md
 end
 
